c20d2b28029ef2f78bf877116d9949b36574b64b
Will <swilson.carlson@hotmail.com>
2022-06-30 14:07:13 -0400

    added 6 slots

---
+++ README.md
@@ -0,0 +1,5 @@
+When entering a Pokemon number, that is one or two digits, it should work as well as it does when entering a number of three digits.
+When the user has added 6 Pokemon to their roster, do not allow them to add any more.
+Allow the user to remove a pokemon from their roster.
+When the user tries to remove a pokemon, make them confirm that they want to remove it before it removes that pokemon.
+In the Pokemon roster section, allow the user to nickname Pokemon in their roster. The user interface (webpage) should display the nickname instead of the Pokemon name if the Pokemon has a nickname.
---
+++ main.js
@@ -0,0 +1,104 @@
+//const pokemon = ['charmander', 'bulbasaur', 'squirtle'];
+
+// const pokemonIDs = ['002', '002', '003'];
+
+const pokemon = [
+    {name: 'Bulbasaur', id: '001'},
+    {name: 'Charmander', id: '004'},
+    {name: 'Squirtle', id: '007'}
+]
+
+
+const userRoster = []
+
+const containerDiv = document.querySelector('#container')
+const newBtn = document.querySelector('#new-pokemon-btn')
+const rosterDiv = document.querySelector('#roster')
+
+/*
+const toNumber (number) => {
+    parseInt(number)
+
+}
+*/
+
+
+// th edata url is the only url. 003 isn't working. 
+
+newBtn.addEventListener('click', async () => {
+  let num = prompt('ENTER A POKEMON NUMBER')
+  console.log('Number entered', num)
+
+
+  var zeroFilled = ('000'+num).slice(-3); // from here: https://stackoverflow.com/questions/1267283/how-can-i-pad-a-value-with-leading-zeros
+//   num.padStart(3,'0') >>> this is another way
+
+  let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${zeroFilled}.png` // how to make it read '5' rather than '005'? how about if numdigits is >1, and first digit = 0, to remove first digit, etc? >> see the zeroFilled variable
+  let dataUrl = `https://pokeapi.co/api/v2/pokemon/${parseInt(num)}` // parseInt makes it so that 003 is read as 3, so that the API is called correctly.
+  let req = await fetch(dataUrl)
+  let res = await req.json()
+  let name = res.forms[0].name
+  console.log(res);
+  let audioUrl = `https://play.pokemonshowdown.com/audio/cries/${name.toLowerCase()}.mp3`
+  let audio = document.createElement('audio')
+  let source = document.createElement('source')
+  source.setAttribute('src', audioUrl)
+  source.setAttribute('type', 'audio/mpeg')
+  audio.append(source)
+  let h3 = document.createElement('h3')
+  h3.innerText = name
+  let img = document.createElement('img')
+  img.setAttribute('src', imgUrl)
+  img.setAttribute('class', 'roster-img')
+  let position = document.querySelector(`#pokemon-${userRoster.length + 1}`)
+  position.addEventListener('click', () => {
+    audio.play()
+  })
+  position.append(img, h3, audio)
+  userRoster.push(num)
+})
+
+
+
+pokemon.map((element, index) => {
+    console.log(element)
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${element.id}.png`
+    let div = document.createElement('div') // adding a div to house the img element
+    let h3 = document.createElement('h3')
+    h3.innerText = element.name
+    div.setAttribute('class', 'pokemon-card') // setting the class of the div
+    let img = document.createElement('img') // creating the img element itself
+    let audioUrl = `https://play.pokemonshowdown.com/audio/cries/${element.name.toLowerCase()}.mp3`
+    let audio = document.createElement('audio') // creating the audio element
+    let source = document.createElement('source') // creating the source element
+    source.setAttribute('src', audioUrl) // setting the src attribute of the source element to = the audiourl
+    source.setAttribute('type', 'audio/mpeg') // setting the type attribute of the source element 
+    audio.append(source) // appending the source element to the audio element
+    div.addEventListener('click', () => {
+      console.log('audio', audioUrl)
+      audio.play()
+    })
+    img.src = imgUrl
+    div.append(img, h3, audio) // append the img, h3, and audio elements to the div
+    containerDiv.append(div)
+  })
+  
+
+
+
+
+/*
+pokemonIDs.map((id) => {
+    console.log(id)
+    console.log(index)
+
+    let imgUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/full/${id}.png`
+    console.log('img url', imgUrl)
+    let img = document.createElement(`img`)
+    img.src = imgUrl
+    img.setAttribute('height', '100px')
+    img.setAttribute('width', '100px')
+    document.body.append(img)
+})
+
+*/
